9a17289ebdeb8d0ca49746d3c8b04e5b28a0b373
gokulajmech <85476372+gokulajmech@users.noreply.github.com>
2021-12-19 14:43:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -101,7 +101,7 @@
            
         
        }
-       console.log(label.valueOf());
+       console.log(labels);
      } else {
        appendPre('No Labels found.');
      }
@@ -112,7 +112,7 @@
 const read= async ()=>
 {
   try{
-    await fetch(' https://gmail.googleapis.com/gmail/v1/users/gokulrajana@gmail.com/messages/inbox',{method:"GET",
+    await fetch(' https://gmail.googleapis.com/gmail/v1/users/gokulrajana@gmail.com/messages',{method:"GET",
   headers:{ OAuth2accesstoken:CLIENT_ID,
     mode : 'no-cors'}}).
     then((res)=>res.json()).
