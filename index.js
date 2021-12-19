0e17dce6322981bf8c6e774b112d16a071f0a92b
gokulajmech <85476372+gokulajmech@users.noreply.github.com>
2021-12-19 14:10:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -111,7 +111,7 @@
 const read= async ()=>
 {
   try{
-    await fetch('GET',' https://gmail.googleapis.com/gmail/v1/users/gokulrajana@gmail.com/messages/inbox').
+    await fetch(' https://gmail.googleapis.com/gmail/v1/users/gokulrajana@gmail.com/messages/inbox',{method:"GET"}).
     then((res)=>res.json()).
     then((res)=>console.log(res));
   }
